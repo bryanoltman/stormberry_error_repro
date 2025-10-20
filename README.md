68db6eda8c49bfbe268cffd47de92cca3243a856
Bryan Oltman <bryan@shorebird.dev>
2025-10-20 12:34:05 -0400

    cleanup, readme

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-A sample command-line application with an entrypoint in `bin/`, library code
-in `lib/`, and example unit test in `test/`.
+# Stormberry Error Repro
+
+## Prereq
+
+Set up a containerized database using:
+
+```
+docker pull postgres
+docker run \
+    --name stormberry_demo \
+    -p 127.0.0.1:5432:5432 \
+    -e POSTGRES_USER=postgres \
+    -e POSTGRES_DB=stormberry_demo \
+    -e POSTGRES_PASSWORD=password \
+    -d \
+    postgres
+```
+
+After cloning, run `bin/stormberry_err_repro`.
+
+Note that all posts are created with the same `createdAt` DateTime. When
+comparing a Post retrieved through a User (i.e., `user.posts.first`) will have a
+non-UTC createdAt, while a Post retrieved directly by id will have a UTC
+createdAt, meaning the same object will be different depending on how it is
+retrieved.
--- bin/stormberry_err_repro.dart
+++ bin/stormberry_err_repro.dart
@@ -14,15 +14,14 @@ void main(List<String> arguments) async {
   await migrate(db);
   await db.users.deleteOne('1');
   await db.posts.deleteMany(['1', '2', '3']);
+  final creationDate = DateTime.now();
 
-  await db.users.insertOne(
-    UserInsertRequest(id: '1', createdAt: DateTime.now()),
-  );
+  await db.users.insertOne(UserInsertRequest(id: '1', createdAt: creationDate));
   await db.posts.insertOne(
     PostInsertRequest(
       id: '1',
       content: 'Hello, world!',
-      createdAt: DateTime.now(),
+      createdAt: creationDate,
       userId: '1',
     ),
   );
@@ -30,7 +29,7 @@ void main(List<String> arguments) async {
     PostInsertRequest(
       id: '2',
       content: 'Hello, world!',
-      createdAt: DateTime.now(),
+      createdAt: creationDate,
       userId: '1',
     ),
   );
@@ -38,7 +37,7 @@ void main(List<String> arguments) async {
     PostInsertRequest(
       id: '3',
       content: 'Hello, world!',
-      createdAt: DateTime.now(),
+      createdAt: creationDate,
       userId: '1',
     ),
   );
@@ -48,6 +47,12 @@ void main(List<String> arguments) async {
     'user!.posts.first.createdAt: ${user!.posts.first.createdAt} isUTC? ${user.posts.first.createdAt.isUtc}',
   );
 
-  final post = await db.posts.queryPost('1');
-  print('post!.createdAt: ${post!.createdAt} isUTC? ${post.createdAt.isUtc}');
+  final firstUserPost = user.posts.first;
+  final firstPost = await db.posts.queryPost('1');
+  print(
+    'post!.createdAt: ${firstPost!.createdAt} isUTC? ${firstPost.createdAt.isUtc}',
+  );
+  print('dates are equal? ${firstUserPost.createdAt == firstPost.createdAt}');
+
+  await db.close();
 }
